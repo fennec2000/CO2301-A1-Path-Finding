25e516998055c824178c2421fca6ea9aaadc7a05
fennec2000 <stuart.alh@gmail.com>
2017-12-04 08:48:53 +0000

    Base files

---
+++ CO2301 Path-Finding/CO2301 Path-Finding.cpp
@@ -0,0 +1,32 @@
+// CO2301 Path-Finding.cpp: A program using the TL-Engine
+
+#include <TL-Engine.h>	// TL-Engine include file and namespace
+#include <vector> // vectors
+using namespace tle;
+
+void main()
+{
+    // Create a 3D engine (using TLX engine here) and open a window for it
+    I3DEngine* myEngine = New3DEngine(kTLX);
+    myEngine->StartWindowed();
+
+    // Add default folder for meshes and other media
+    myEngine->AddMediaFolder("C:\\ProgramData\\TL-Engine\\Media");
+
+    /**** Set up your scene here ****/
+    vector<vector<int>> map; // the current map
+
+
+    // The main game loop, repeat until engine is stopped
+    while (myEngine->IsRunning())
+    {
+        // Draw the scene
+        myEngine->DrawScene();
+
+        /**** Update your scene each frame here ****/
+
+    }
+
+    // Delete the 3D engine now we are finished with it
+    myEngine->Delete();
+}
